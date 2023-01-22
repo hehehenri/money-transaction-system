252fb6027738a364d7254ee23f95a3a3a7cfb866
henri <henri1i.dev@gmail.com>
2023-01-22 18:19:26 -0300

    test: create unit tests for cpf value object

--- src/Customer/Domain/ValueObjects/CPF.php
+++ src/Customer/Domain/ValueObjects/CPF.php
@@ -8,14 +8,22 @@ use Src\User\Domain\ValueObjects\Document;
 
 class CPF extends Document
 {
+    /** @throws InvalidParameterException */
     public function __construct(string $value)
     {
+        $value = $this->removeNonNumericChars($value);
+
         $validator = new CPFValidator();
 
         if (! $validator->validate($value)) {
-            InvalidParameterException::invalidCPF($value);
+            throw InvalidParameterException::invalidCPF($value);
         }
 
         parent::__construct($value);
+    }
+
+    private function removeNonNumericChars(string $value): string
+    {
+        return preg_replace('/[^0-9]/', '', $value) ?? '';
     }
 }
---
+++ tests/Unit/Customer/ValueObjects/CPFTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Customer\ValueObjects;
+
+use Src\Customer\Domain\InvalidParameterException;
+use Src\Customer\Domain\ValueObjects\CPF;
+use Tests\TestCase;
+
+class CPFTest extends TestCase
+{
+    /** @dataProvider validCPFs */
+    public function testTheCPFIsValid(string $payload): void
+    {
+        $cpf = new CPF($payload);
+
+        $this->assertEquals($this->removePonctuation($payload), (string) $cpf);
+    }
+
+    /** @dataProvider invalidCPFs */
+    public function testTheCPFIsInvalid(string $payload): void
+    {
+        $this->expectException(InvalidParameterException::class);
+
+        new CPF($payload);
+    }
+
+    /** @return array<string, array<string>> */
+    private function validCPFs(): array
+    {
+        return [
+            'starting_with_zero' => ['04784261001'],
+            'with_ponctuation' => ['047.842.610-01'],
+            'regular_one' => ['19247072093'],
+            'regular_two' => ['45804352067'],
+            'regular_three' => ['77143589047'],
+        ];
+    }
+
+    /** @return array<string, array<string>> */
+    private function invalidCPFs(): array
+    {
+        return [
+            'too_small' => ['1924707'],
+            'too_long' => ['1924707209312'],
+            'invalid' => ['19247075093'],
+        ];
+    }
+
+    private function removePonctuation(string $payload): string
+    {
+
+        return preg_replace('/[^0-9]/', '', $payload) ?? '';
+    }
+}
