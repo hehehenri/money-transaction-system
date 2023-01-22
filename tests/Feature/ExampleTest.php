308500024534b0d42f4b145f44f549f8b841287e
henri <henri1i.dev@gmail.com>
2023-01-21 22:19:41 -0300

    build: add example test to see if pipeline passes it

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testOk(): void
+    {
+        $this->expectNotToPerformAssertions();
+    }
+}
