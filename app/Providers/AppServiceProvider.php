689ea078c9fa5e163cd5368aeef1ae93a4f827e4
henri <henri1i.dev@gmail.com>
2023-01-21 21:10:00 -0300

    fix: remove old router

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Src\Presentation\Rest\Router;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->register(Router::class);
+        //
     }
 
     /**
