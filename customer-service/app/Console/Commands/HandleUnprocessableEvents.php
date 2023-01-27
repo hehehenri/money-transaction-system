050c69dd3bcd041626c13b563e2d85d5df83a7f2
henri <henri1i.dev@gmail.com>
2023-01-27 01:27:01 -0300

    feat: create command to handle unprocessed events

---
+++ customer-service/app/Console/Commands/HandleUnprocessableEvents.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Src\Infrastructure\Events\Repositories\EventRepository;
+use Src\Infrastructure\Events\ValueObjects\EventType;
+
+class HandleUnprocessableEvents extends Command
+{
+    protected $signature = 'events:handle';
+
+    protected $description = 'Handle unprocessed events';
+
+    public function handle(EventRepository $repository): int
+    {
+        $unprocessedEvents = $repository->getUnprocessed();
+
+        foreach (EventType::cases() as $type) {
+            $events = $unprocessedEvents->get($type->value);
+
+            $type->handler()->handle($events);
+        }
+
+        return Command::SUCCESS;
+    }
+}
